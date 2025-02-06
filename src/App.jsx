e8b4d29080f7769cf9d14a172667b8ce9a37b18a
방은혜 <grace01099139607@gmail.com>
2025-02-07 01:50:23 +0900

    RootLayout 컴포넌트 추가 및 기본 레이아웃 구조 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import TodoList from "./components/TodoList";
+import RootLayout from "./components/layout/RootLayout";
+import TodoContainer from "./components/todo/Todocontainer";
 
 const App = () => {
   return (
-    <main className="main-center">
-      <TodoList />
-    </main>
+    <RootLayout>
+      <TodoContainer />
+    </RootLayout>
   );
 };
 
---
+++ src/components/layout/RootLayout.jsx
@@ -0,0 +1,18 @@
+//Compositional Layout
+const RootLayout = ({ children }) => {
+  return (
+    <main>
+      <header>
+        <h1>Header</h1>
+      </header>
+
+      <div>{children}t</div>
+
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </main>
+  );
+};
+
+export default RootLayout;
