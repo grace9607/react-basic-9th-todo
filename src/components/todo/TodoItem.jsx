0705a585de6da57d82be2e5a6bb5ff1e8c9e1b22
방은혜 <grace01099139607@gmail.com>
2025-02-07 10:17:52 +0900

    feat: TodoItem 컴포넌트 추가 및 TodoList에서 분리

---
+++ src/components/todo/TodoItem.jsx
@@ -0,0 +1,29 @@
+const TodoItem = ({
+  completed,
+  text,
+  handleToggleCompleted,
+  handleDelete,
+  id,
+}) => {
+  return (
+    <li>
+      <p
+        style={{
+          textDecoration: completed ? "line-through" : "none",
+        }}
+      >
+        {text}
+      </p>
+
+      <div>
+        <button onClick={() => handleToggleCompleted(id)}>
+          {completed ? "취소하기" : "완료하기"}
+        </button>
+
+        <button onClick={() => handleDelete(id)}>삭제하기</button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
