14a401aae70935279c0a2c7ddb71a5bcd21fead7
방은혜 <grace01099139607@gmail.com>
2025-02-14 00:36:15 +0900

    feat: TodoProvider 추가 및 샘플 할 일 데이터 상수화, TodoDetailPage에서 할 일 상세 정보 표시

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "./components/layout/RootLayout";
 import HomePage from "./pages/HomePage";
 import TodoDetailPage from "./pages/TodoDetailPage";
+import TodoProvider from "./components/provider/TodoProvider";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="todos/:id" element={<TodoDetailPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <TodoProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="todos/:id" element={<TodoDetailPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TodoProvider>
   );
 };
 
--- src/pages/TodoDetailPage.jsx
+++ src/pages/TodoDetailPage.jsx
@@ -1,8 +1,26 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
+import { SAMPLE_TODOS } from "../constants/sample-todos";
+import TodoItem, { ActionButton } from "../components/todo/TodoItem";
+// import { styled } from "styled-components";
 
 const TodoDetailPage = () => {
   const { id } = useParams();
-  return <div>TodoDetailPage ID : {id}</div>;
+
+  const targetTodoItem = SAMPLE_TODOS.find((todo) => todo.id === id);
+
+  return (
+    <div>
+      <TodoItem
+        id={targetTodoItem.id}
+        text={targetTodoItem.text}
+        completed={targetTodoItem.com}
+      />
+
+      <ActionButton $bgColor="#242424">
+        <Link to="/">돌아가기</Link>
+      </ActionButton>
+    </div>
+  );
 };
 
 export default TodoDetailPage;
